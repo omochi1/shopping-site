250aef40a5a34653d5a42d6029fbc80e19431ee3
kawadamaya <kawadamaya0222@gmail.com>
2017-03-27 12:56:08 +0000

    test

---
+++ shopping-site/test-carousel.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+           loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+            
+        }
+        );
+});
+
+
+$(document).ready(function(){
+  $("#sp_menu_btn").click(function () {
+    $(".toggle").slideToggle();
+  });
+});
+
+$(window).load(function(){
+  $("#navi img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_photo>img").attr("src", img_src);
+    return false;
+  });
+});
+
